643b46c5d3bc052622e14cc3119212db75b800e4
willandru <willandru@hotmail.com>
2021-12-02 14:27:20 -0500

    HTML templates using JINJA

--- FLASK/WebSite/auth.py
+++ FLASK/WebSite/auth.py
@@ -1,3 +1,16 @@
 from flask import Blueprint
+from flask.templating import render_template
 
 auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return render_template("login.html")
+
+@auth.route('/logout')
+def logout():
+    return render_template("home.html")
+
+@auth.route('/sign-up')
+def sign_up():
+    return render_template("signup.html")
